0c6496d4892c229b8078de678fbbcc2aa4d1780e
tinAH707 <csmat06@gmail.com>
2026-04-10 21:39:49 -0400

    Add Content + Start Styles
    Adding images and fixed up layout in html. Start on styles. Added electron and nodesjs to help project be a seperate desktop application rather than a window.

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 397,
+    height: 587,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: true, 
+    transparent: false,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ scripts.js
@@ -0,0 +1,1 @@
+//any functions for the actual app
