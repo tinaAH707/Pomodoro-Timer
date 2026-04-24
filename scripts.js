671ebc5b230b03dd09e59bcd86a51b119c7898f3
tinAH707 <csmat06@gmail.com>
2026-04-23 21:09:44 -0400

    Create Functional Task List
    Used if statment to add list item, used addEventListner and .target to toggle checked and unchecked list item as well as removing list item, added local storage for list items to be saved. Created a serperate intialize function for items

--- scripts.js
+++ scripts.js
@@ -1,131 +1,79 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    //if you click unselcted and its unselcted then it turns into selected and the other turns unselcted
-//look in octoggle, if i click on akiselcted, then hide mimiselcted
-    // const selectOC = (octoggle) =>{
-    //     const akiDiv = document.getElementsByClassName('aki');
-    //     const mimiDiv = document.getElementsByClassName('mimi');
-    //     const akiSelected = document.getElementById('aki-selected');
-    //     const akiUnselected = document.getElementsByClassName('aki-unselected');
-    //     const mimiSelected = document.getElementById('mimi-selected');
-    //     const mimiUnselected = document.getElementsByClassName('mimi-unselected');
-    //     //look at the classes and if i click selcted in a certin class, change it to that image and hide the other
-        
-    //     akiDiv.addEventListener('click', (octoggle)=>{
-    //         akiSelected.classList.add('unselected');
-            
-    //     });
-       
-    
-    // }
-        const toggleDiv = document.getElementsByClassName('oc-toggle');
-        const akiDiv = document.getElementsByClassName('aki');
-        const mimiDiv = document.getElementsByClassName('mimi');
+//TASK BOX 
+//===================================================================
+    const toggleDiv = document.getElementsByClassName('oc-toggle');
+    const akiDiv = document.getElementsByClassName('aki');
+    const mimiDiv = document.getElementsByClassName('mimi');
       
+    const akiUnselcted = document.getElementById('aki-unselected');
+    const mimiSelcted = document.getElementById('mimi-selected');
 
-        const akiUnselcted = document.getElementById('aki-unselected');
-        // const akiSelcted = document.getElementById('aki-selected');
-        // const mimiUnselcted = document.getElementById('mimi-unselected');
-        const mimiSelcted = document.getElementById('mimi-selected');
+    const mimiGif = document.getElementById('mimi-animation');
+    const taskBox = document.getElementById('input-box');
+    const listContainer = document.getElementById('list-container');
+    const button = document.getElementById('add-task-button');
 
-    //    const akiGif = document.getElementById('aki-animation');
-       const mimiGif = document.getElementById('mimi-animation');
+    //intialize
+    document.addEventListener('DOMContentLoaded', () => {
+        mimiSelcted.addEventListener("click", (mimi));
+        akiUnselcted.addEventListener("click", (aki));
+        button.addEventListener("click", (addTask));
 
-    
-
-   
-        
-        //   function toggleAki(){
-        //     akiUnselcted.addEventListener("click", () =>{
-        //         if(akiUnselcted.src.match('aki-Unclick.png')){
-        //             akiUnselcted.setAttribute('src', "content/images/aki-clicked.png");
-        //             mimiSelcted.setAttribute('src', "content/images/mimi-UNclick.png");
-                    
-        //             mimiGif.setAttribute('src', "content/animation/aki-animation.gif");
-        //         } else(mimiUnselcted.addEventListener("click", () =>{
-        //                 akiUnselcted.setAttribute('src', "content/images/aki-Unclick.png");
-        //                 mimiSelcted.setAttribute('src', "content/images/mimi-UNclick.png");
-                        
-        //                 mimiGif.setAttribute('src', "content/animation/aki-animation.gif");
-        //         }));
-               
-        //     })
-        //   };---------------
-
-           
-        
-            akiUnselcted.addEventListener("click", () =>{
-                    akiUnselcted.setAttribute('src', "content/images/aki-clicked.png");
-                    mimiSelcted.setAttribute('src', "content/images/mimi-UNclick.png");
-                    
-                    mimiGif.setAttribute('src', "content/animation/aki-animation.gif");
-            })
-
-            mimiSelcted.addEventListener("click", () =>{
-                        
-                    akiUnselcted.setAttribute('src', "content/images/aki-Unclick.png");
-                    mimiSelcted.setAttribute('src', "content/images/mimi-Clicked.png");
-                    
-                    mimiGif.setAttribute('src', "content/animation/mimi-animation.gif");
-            });
-
-
-
-        
-
-        //   function toggleMimi(){
-        //     mimiUnselcted.addEventListener("click", () =>{
-        //         if(mimiUnselcted.src.match('mimi-UNclick.png'));{
-        //             akiSelcted.setAttribute('src', "content/images/aki-Unclick.png");
-        //             mimiUnselcted.setAttribute('src', "content/images/mimi-Clicked.png");
-        //         };
-        //     })
-        //   }
-
+    });
          
 
-          
-          
 
-        
-
-
-    //===================================================================
-    const taskInput = document.getElementById('task-input');
-    const addTaskBtn = document.getElementById('add-task-button');
-    const tasklist = document.getElementById('task-list');
-    //when you type into the input area it -> click addtask, and then it will be added into task
-
-    
-    const addTask = (event) => {
+//taskbox
+    function addTask(){
         event.preventDefault();
 
-        const taskText = taskInput.value.trim();
-        if(!taskText){
-            return;
+        if(taskBox.value === ""){
+            alert("add a freaking task >:3 !");
+        } else{
+            let li = document.createElement("li");
+            li.innerHTML = taskBox.value;
+            listContainer.appendChild(li);  
+
+            let span = document.createElement("span");
+            span.innerHTML="\u00d7";
+            li.appendChild(span);
         }
+        taskBox.value = '';
+        saveData(); //after task is called it will save it
+    };
+          
 
-        const li = document.createElement('li');
-        li.textContent = taskText;
-        li.innerHTML = `
-        <input type="checkbox" class="checkbox">
-        <span>${taskText}</span>
-        `
+    listContainer.addEventListener("click", function(e){
+        if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked"); //clicked the li item and it will check it
+            saveData();
+        }
+        else if(e.target.tagName === "SPAN"){ //click the x and it will remove the element
+            e.target.parentElement.remove();
+            saveData();
+        }
+    
+    }, false);
 
-
-
-        tasklist.appendChild(li);
-        taskInput.value = '';
-
+    function saveData(){
+        localStorage.setItem("data", listContainer.innerHTML);
     }
 
-    addTaskBtn.addEventListener('click', addTask);
-    taskInput.addEventListener('keypress', (e) =>{
-        if (e.key === 'Enter'){
-            addTask(e);
-        }
-    })
+    function showTask(){
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+   showTask();
 
 
-});
+    function mimi(){
+        akiUnselcted.setAttribute('src', "content/images/aki-Unclick.png");
+        mimiSelcted.setAttribute('src', "content/images/mimi-Clicked.png");
+                    
+        mimiGif.setAttribute('src', "content/animation/mimi-animation.gif");
+    }
 
+    function aki(){
+        akiUnselcted.setAttribute('src', "content/images/aki-clicked.png");
+        mimiSelcted.setAttribute('src', "content/images/mimi-UNclick.png");
+                
+         mimiGif.setAttribute('src', "content/animation/aki-animation.gif");
+    }
