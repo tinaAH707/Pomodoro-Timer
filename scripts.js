9caa3d2e36f333c4a1853b04da4a5031bde0b8e9
tinAH707 <csmat06@gmail.com>
2026-04-24 21:46:42 -0400

    Fixed JS for OC toggle click and hover state

--- scripts.js
+++ scripts.js
@@ -4,27 +4,52 @@
     const akiDiv = document.getElementsByClassName('aki');
     const mimiDiv = document.getElementsByClassName('mimi');
       
-    const akiUnselcted = document.getElementById('aki-unselected');
-    const mimiSelcted = document.getElementById('mimi-selected');
-
-    const mimiGif = document.getElementById('mimi-animation');
     const taskBox = document.getElementById('input-box');
     const listContainer = document.getElementById('list-container');
-    const button = document.getElementById('add-task-button');
+    const taskButton = document.getElementById('add-task-button');
 
-    //intialize
+    const akiUnselcted = document.getElementById('aki-unselected');
+    const akiSelcted = document.getElementById('aki-selected');
+
+    const mimiSelcted = document.getElementById('mimi-selected');
+    const mimiUnselcted = document.getElementById('mimi-unselected');
+
+    const mimiGif = document.getElementById('mimi-animation');
+    const akiGif = document.getElementById('aki-animation');
+
+// Initilize ====================================================
     document.addEventListener('DOMContentLoaded', () => {
-        mimiSelcted.addEventListener("click", (mimi));
-        akiUnselcted.addEventListener("click", (aki));
-        button.addEventListener("click", (addTask));
-
+        taskButton.addEventListener("click", (addTask));
+        akiSelect();
+        mimiSelect();
     });
-         
 
 
-//taskbox
+
+// Sounds ====================================================
+    function soundClick(){
+        const clickS = new Audio("content/audio/DDLC_Click.mp3");
+        clickS.play();
+    }
+
+    function soundHover(){
+        const hoverS = new Audio("content/audio/DDLC_hover.mp3");
+        taskButton.addEventListener("mouseenter", () =>{
+            hoverS.play();
+        });
+    }
+
+    function soundDone(){
+        const doneS = new Audio("content/audio/Picnic_Heal_Omori.mp3");
+        doneS.play();
+    }
+
+
+
+// Task Box ====================================================
     function addTask(){
         event.preventDefault();
+        
 
         if(taskBox.value === ""){
             alert("add a freaking task >:3 !");
@@ -36,23 +61,27 @@
             let span = document.createElement("span");
             span.innerHTML="\u00d7";
             li.appendChild(span);
+            soundClick();
         }
         taskBox.value = '';
-        saveData(); //after task is called it will save it
+        saveData(); //after task is called it will save it        
     };
-          
+         
 
     listContainer.addEventListener("click", function(e){
         if(e.target.tagName === "LI"){
             e.target.classList.toggle("checked"); //clicked the li item and it will check it
             saveData();
+            soundDone();
         }
         else if(e.target.tagName === "SPAN"){ //click the x and it will remove the element
             e.target.parentElement.remove();
             saveData();
+            
         }
     
     }, false);
+
 
     function saveData(){
         localStorage.setItem("data", listContainer.innerHTML);
@@ -61,19 +90,92 @@
     function showTask(){
         listContainer.innerHTML = localStorage.getItem("data");
     }
+
    showTask();
 
 
-    function mimi(){
-        akiUnselcted.setAttribute('src', "content/images/aki-Unclick.png");
-        mimiSelcted.setAttribute('src', "content/images/mimi-Clicked.png");
-                    
-        mimiGif.setAttribute('src', "content/animation/mimi-animation.gif");
-    }
 
-    function aki(){
-        akiUnselcted.setAttribute('src', "content/images/aki-clicked.png");
-        mimiSelcted.setAttribute('src', "content/images/mimi-UNclick.png");
-                
-         mimiGif.setAttribute('src', "content/animation/aki-animation.gif");
-    }
+// Select States ====================================================
+   function akiSelect(){
+    akiUnselcted.addEventListener('click', () =>{
+        akiSelcted.classList.remove('selected');
+        akiUnselcted.style.display = 'none';
+
+        mimiSelcted.style.display = 'none';
+        mimiUnselcted.classList.remove('unselected');
+
+        akiGif.classList.remove('aki-anim');
+        mimiGif.style.display = 'none';
+
+        soundClick();
+    })
+    hoverAki();
+
+   }
+
+   function mimiSelect(){
+        
+        mimiUnselcted.addEventListener('click', () =>{
+            mimiSelcted.style.display = 'inline';
+            mimiUnselcted.classList.add('unselected');
+
+            akiSelcted.classList.add('selected');
+            akiUnselcted.style.display = 'inline';
+
+            mimiGif.style.display = 'block';
+            akiGif.classList.add('aki-anim');
+        
+            soundClick();
+        })
+        hovermimi();
+     
+   }
+
+
+
+// Hover States ====================================================
+    function hoverAki(){
+        
+        akiUnselcted.addEventListener('mouseenter', () =>{
+            akiUnselcted.style.transform = 'scale(1.1)';
+        });
+        akiUnselcted.addEventListener('mouseleave', () =>{
+            akiUnselcted.style.transform = 'scale(1)';
+        });
+    };
+
+    function hovermimi(){
+        
+        mimiUnselcted.addEventListener('mouseenter', () =>{
+            mimiUnselcted.style.transform = 'scale(1.1)';
+        });
+        mimiUnselcted.addEventListener('mouseleave', () =>{
+            mimiUnselcted.style.transform = 'scale(1)';
+        });
+    };
+
+//Stop Watch =========================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Stop and Go Buttons =========================================================
+
+
+
+
+
+
+
+
+
